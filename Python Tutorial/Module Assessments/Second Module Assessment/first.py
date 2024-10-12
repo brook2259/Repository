4230c0922ee6e7b8b08289648e97d5f91e66bfe6
brook2259 <brook2@uwo.ca>
2024-10-12 12:06:59 -0400

    Add files via upload

--- Python Tutorial/Module Assessments/Second Module Assessment/first.py
+++ Python Tutorial/Module Assessments/Second Module Assessment/first.py
@@ -1,9 +1,10 @@
 def main():
     f0 = int(input('enter either 0 or 1:'))
     f1 = int(input('enter the number that follows the number you just entered:'))
+    final = int(input('how many fibonacci values would you like to generate? '))
     f2 = [f0,f1]
     
-    for index in range(2,10):
+    for index in range(2,final):
         fn = f2[index-1]+f2[index-2]
         f2.append(fn)
     print(f2)
