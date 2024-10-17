573d7983605f75dcd0519b2a1a35d2fb7d6097bb
brook2259 <brook2@uwo.ca>
2024-10-17 17:54:17 -0400

    Add files via upload

--- Python Tutorial/Module Assessments/Fourth Module Assessment/first.py
+++ Python Tutorial/Module Assessments/Fourth Module Assessment/first.py
@@ -1,1 +1,15 @@
+# Calculate Factorial Using Recursion
 
+def factorial(num):
+    if num==0 or num==1:
+        return 1
+    else:
+        result = num*factorial(num-1)
+    return result
+
+
+num=5
+
+result = factorial(num)
+
+print('the factorial of num is: ', result)
